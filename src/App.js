6ba6c2dde74658c25ec8336d0234b4b2eb413938
YostinaAziz <y.a.aziz@student.aast.edu>
2025-11-12 12:46:59 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,10 +7,13 @@ import SectionE from "./components/sectionE";
 import SectionF from "./components/sectionF";
 import SectionG from "./components/sectionG";
 import SectionH from "./components/sectionH";
+import SectionI from "./components/sectionI";
+import SectionJ from "./components/sectionJ";
+import SectionK from "./components/sectionK";
 
 function App() {
   const [activeSection, setActiveSection] = useState("A");
-
+  
   const [inputs, setInputs] = useState({
     year: 2049,
     population: 6500000,
@@ -34,7 +37,7 @@ function App() {
     <div className="dashboard">
       <aside className="sidebar">
         <h2>Sections</h2>
-        {["A","B","C","D","E","F","G","H"].map(sec => (
+        {["A","B","C","D","E","F","G","H","I","J","K"].map(sec => (
           <button key={sec} onClick={() => setActiveSection(sec)}>
             {`Section ${sec}`}
           </button>
@@ -50,6 +53,33 @@ function App() {
         {activeSection === "F" && <SectionF inputs={inputs} />}
         {activeSection === "G" && <SectionG inputs={inputs} />}
         {activeSection === "H" && <SectionH inputs={inputs} />}
+        {activeSection === "I" && <SectionI inputs={inputs} />}
+        {activeSection === "J" && <SectionJ inputs={inputs} />}
+        {activeSection === "K" && <SectionK flightsBase={{
+    small: 750000,
+    medium: 7500000,
+    large: 22500000,
+  }}
+  flightsUpperSource={{
+    small: 2500000,
+    medium: 25000000,
+    large: 275000000,
+  }}
+  uavsBase={{
+    small: 25000,
+    medium: 25000,
+    large: 2500,
+  }}
+  uavsUpperSource={{
+    small: 87500,
+    medium: 100000,
+    large: 42500,
+  }}
+/>}
+ 
+  
+
+      
       </main>
     </div>
   );
